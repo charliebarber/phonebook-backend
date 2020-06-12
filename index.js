1f09875d207a93e32772a4f6ae0d1b0e6e8dc232
Charlie <charlieb@protonmail.com>
2020-06-12 16:53:06 +0100

    3.4

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const { response } = require('express')
+const app = express()
+
+const persons = [
+    {
+        "name": "Arto Hellas",
+            "number": "040-123456",
+                "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+            "number": "39-44-5323523",
+                "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+            "number": "12-43-234345",
+                "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+            "number": "39-23-6423122",
+                "id": 4
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(`
+    <div>
+        Phonebook has info for ${persons.length} persons
+        <p>${date}</p>
+    </div>
+    `)
+})
+
+app.get('/api/persons/:id', (req,res) => {
+    const id = Number(req.params.id)
+    const person = persons.find((person) => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Sever running on ${PORT}`)
+})
