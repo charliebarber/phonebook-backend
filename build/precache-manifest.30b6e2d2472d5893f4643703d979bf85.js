eaf092d8315f3a7f351ca54f941ece15c8f5b91b
Charlie <charlieb@protonmail.com>
2020-06-12 21:35:07 +0100

    uibuild

---
+++ build/precache-manifest.30b6e2d2472d5893f4643703d979bf85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07c29017a75e161cda35ea797cfd852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c1a548b62240fc04c5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fdc00f3d2f4001d77518",
+    "url": "/static/js/2.9d147208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d147208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c1a548b62240fc04c5",
+    "url": "/static/js/main.944e8b52.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
